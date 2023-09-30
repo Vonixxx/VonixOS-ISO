d522dae6f95e508b1d7aa7a06d2c09029a968705
Vonixxx <vonixxxwork@tuta.io>
2023-09-30 10:22:00 +0200

    added iso to repository

---
+++ vonix.nix
@@ -0,0 +1,30 @@
+# Minimal NixOS 
+{ config, pkgs, ... }:
+
+
+{
+ imports = [
+   <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+ ];
+
+ environment.systemPackages = with pkgs; [
+   git
+   wget
+   python3Full
+ ];
+ 
+ nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+ systemd.services.fetchScript = {
+   wantedBy    = [ "multi-user.target" ];
+   wants       = [ "network-online.target" ];
+   after       = [ "network-online.target" ];
+   description = "Fetch and Run Python Script from GitHub";
+   serviceConfig = {
+     Type = "oneshot";
+     ExecStart = ''
+        /bin/sh -c "cd /home/nixos && /run/current-system/sw/bin/wget https://github.com/Vonixxx/NixOS/raw/main/iso/automated-install.py -O automated-install.py && chmod +x automated-install.py"
+     '';
+   };
+ };
+}
