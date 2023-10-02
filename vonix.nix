957561f4a6593f09fe26eb83b086fa7d514075cf
Vonixxx <vonixxxwork@tuta.io>
2023-10-02 20:32:52 +0200

    modified iso configuration for easier readability

--- vonix.nix
+++ vonix.nix
@@ -1,6 +1,9 @@
-# Minimal NixOS 
+###########
+# VonixOS #
+#################
+# Minimal NixOS # 
+#################
 { config, pkgs, ... }:
-
 
 {
  imports = [
@@ -23,7 +26,12 @@
    serviceConfig = {
      Type = "oneshot";
      ExecStart = ''
-        /bin/sh -c "cd /home/nixos && /run/current-system/sw/bin/wget https://github.com/Vonixxx/VonixOS/raw/main/install-script/automated-install.py -O automated-install.py && chmod +x automated-install.py"
+        /bin/sh -c "
+         cd /home/nixos \
+         && /run/current-system/sw/bin/wget https://github.com/Vonixxx/VonixOS/raw/main/install-script/main.py -O main.py \
+         && /run/current-system/sw/bin/wget https://github.com/Vonixxx/VonixOS/raw/main/install-script/functions.py -O functions.py \
+         && chmod +x main.py
+        "
      '';
    };
  };
