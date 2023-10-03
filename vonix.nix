a5c97b4ce3b12c549de2410903904fb0a4bbfe28
Vonixxx <vonixxxwork@tuta.io>
2023-10-03 11:34:12 +0200

    modified iso

--- vonix.nix
+++ vonix.nix
@@ -27,12 +27,12 @@
    serviceConfig = {
      Type = "oneshot";
      ExecStart = ''
-        /bin/sh -c "
-         cd /home/nixos \
-         && /run/current-system/sw/bin/wget https://github.com/Vonixxx/VonixOS/raw/main/install-script/main.py -O main.py \
-         && /run/current-system/sw/bin/wget https://github.com/Vonixxx/VonixOS/raw/main/install-script/functions.py -O functions.py \
-         && chmod +x main.py
-        "
+        /bin/sh -c '\
+         cd /home/nixos && \
+         /run/current-system/sw/bin/wget https://github.com/Vonixxx/VonixOS/raw/main/install-script/main.py -O main.py && \
+         /run/current-system/sw/bin/wget https://github.com/Vonixxx/VonixOS/raw/main/install-script/functions.py -O functions.py && \
+         chmod +x main.py
+        '
      '';
    };
  };
